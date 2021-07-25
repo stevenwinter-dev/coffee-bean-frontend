5b0dc10c0a9a4fb547715c67ae06df9eeb679df4
steveplayshorn <steveplayshorn@gmail.com>
2021-07-25 17:43:06 -0400

    Completed styling for shopping cart.

--- Frontend/src/components/Cart/Cart.js
+++ Frontend/src/components/Cart/Cart.js
@@ -7,9 +7,14 @@ import cartData from '../../cartData'
 const Cart = () => {
     const [cart, setCart] = useState(cartData)
     return (
-        <main className='cart'>
-            <h2>Shopping Cart</h2>
-            {cart.map(item => <CartItem item={item} key={item.id} />)}
+        <main>
+            <div className="cart">
+                <h2>Shopping Cart</h2>
+                {cart.map(item => <CartItem item={item} key={item.id} />)}
+                <form className="checkout-btn-container" action="/api/create-checkout-session" method="POST">
+                    <button className="checkout-btn" type="submit">Checkout</button>
+                </form>
+            </div>
         </main>
     )
 }
