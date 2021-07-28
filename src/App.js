4f025d5f6a5a510d5cf92a4c366032193228d7fc
steveplayshorn <steveplayshorn@gmail.com>
2021-07-28 19:56:34 -0400

    Setup.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './App.css';
 import Coffees from './components/Coffees/Coffees';
 import Hero from './components/Hero/Hero';
 import Nav from './components/Nav/Nav';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import coffeesData from './coffeesData';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import CoffeeDetails from './components/CoffeeDetails/CoffeeDetails';
 import Cart from './components/Cart/Cart';
 import Footer from './components/Footer/Footer';
+import axios from 'axios'
 
 function App() {
 
   const [coffees, setCoffees] = useState(coffeesData)
   
+  useEffect(() => {
+    (async () => {
+        const result = await axios(`/api/index`)  
+      setCoffees(result.data)
+    })()
+  }, [])
+
   return (
     <Router className="App">
       <Nav />
