64f97bc4271d6b7c7b901685579e64dd26e0cae0
sali1993 <salahudeenali28@gmail.com>
2021-07-29 17:37:02 -0400

    auth setup & css

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,15 +5,26 @@ import Hero from './components/Hero/Hero';
 import Nav from './components/Nav/Nav';
 import { useState, useEffect } from 'react'
 import coffeesData from './coffeesData';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import CoffeeDetails from './components/CoffeeDetails/CoffeeDetails';
 import Cart from './components/Cart/Cart';
 import Footer from './components/Footer/Footer';
 import { loadStripe } from '@stripe/stripe-js'
 import { Elements } from '@stripe/react-stripe-js'
+<<<<<<< HEAD
 import Coffee from './components/Coffee/Coffee';
 import axios from 'axios';
 import AddCoffee from './components/AddCoffee/AddCoffee';
+=======
+import { AuthProvider } from './context/AuthContext';
+import Signup from "./components/Auth/Signup";
+import Login from "./components/Auth/Login";
+import ForgotPassword from "./components/Auth/ForgotPassword";
+import UpdateProfile from "./components/Auth/UpdateProfile";
+import Dashboard from './components/Auth/Dashboard';
+import PrivateRoute from "./components/Auth/PrivateRoute"
+
+>>>>>>> 30747b3 (auth setup & css)
 
 const stripePromise = loadStripe(`${process.env.PUBLISHABLE_KEY}`)
 
@@ -39,7 +50,15 @@ function App() {
         <Route path='/' exact render={props => <Coffees coffees={coffees} /> }/>
         <Route path='/coffee/:id' render={props => <CoffeeDetails match={props.match} />} />
         <Route path='/cart' exact component={Cart} />
+<<<<<<< HEAD
         <Route path='/dashboard/new' exact component={AddCoffee} />
+=======
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/update-profile" component={UpdateProfile} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/forgot-password" component={ForgotPassword} />
+>>>>>>> 30747b3 (auth setup & css)
         <Footer />
       </Router>
     </Elements>
