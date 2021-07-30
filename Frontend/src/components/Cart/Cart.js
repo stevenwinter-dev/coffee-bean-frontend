13ac85c74db4268413fd55cc856fd0aff2a4db77
steveplayshorn <steveplayshorn@gmail.com>
2021-07-30 17:46:37 -0500

    Removed shipping info form as Stripe handles that.

--- Frontend/src/components/Cart/Cart.js
+++ Frontend/src/components/Cart/Cart.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 const Cart = () => {
     const [cart, setCart] = useState(cartData)
-    const [checkingOut, setCheckingOut] = useState(false)
+   
     const [isProcessing, setProcessingTo] = useState(false);
     const [checkoutError, setCheckoutError] = useState();
 
@@ -17,26 +17,8 @@ const Cart = () => {
 
     const handleFormSubmit = async e => {
         e.preventDefault()
-        
-        const BillingDetails = {
-            name: e.target.name.value,
-            email: e.target.email.value,
-            address: {
-                address: e.target.address.value,
-                city: e.target.city.value,
-                state: e.target.state.value,
-                postal_code: e.target.zip.value
-            }
-        }
-
-        
-}
-
-    const handleCheckout = () => {
-        const form = document.querySelector('.shipping-container')
-        form.style.display = 'block'
-        setCheckingOut(true)
     }
+
 
     const makePayment = token => {
         const body = {
@@ -66,34 +48,14 @@ const Cart = () => {
                 <h2>Shopping Cart</h2>
                 {cart.map(item => <CartItem item={item} key={item.id} />)}
                 <div className="shipping-container">
-                {/* INSERT ON FORM BELOW -onSubmit={handleFormSubmit}  */}
                     <form action="/api/create-checkout-session" onSubmit={handleFormSubmit}  className='shipping-form' >
-                        <label htmlFor="name">Name</label>
-                        <input id="name" type="text" name='name'/>
-                        <label htmlFor="email">Email</label>
-                        <input id="email" type="email" name='email'/>
-                        <label htmlFor="address">Address</label>
-                        <input id="address" type="text" name='address'/>
-                        <label htmlFor="city">City</label>
-                        <input id="city" type="text" name='city'/>
-                        <label htmlFor="state">State</label>
-                        <input id="state" type="text" name='state'/>
-                        <label htmlFor="zip">Zip</label>
-                        <input id="zip" type="number" name='zip'/>
-                        
-                        {/* <input type="submit" value='submit' /> */}
-                        <StripeCheckout stripeKey='pk_test_lOdDQfzqfyxcLovxwkLgniBU' token={makePayment} name='pay' amount={totalPrice * 100}>
+                        <StripeCheckout stripeKey='pk_test_lOdDQfzqfyxcLovxwkLgniBU' token={makePayment} name='pay' amount={totalPrice * 100} shippingAddress billingAddress >
                             <div className="checkout-btn-container">
-                                {/* <button className='checkout-btn'>Buy Now</button> */}
                                 <input className='checkout-btn' type="submit" value='Buy Now' />
                             </div>
                         </StripeCheckout>
                     </form>
-                    
                 </div>
-                {!checkingOut && <div className="checkout-btn-container">
-                    <button className="checkout-btn" onClick={handleCheckout}>Checkout</button>
-                </div>}
             </div>
         </main>
     )
